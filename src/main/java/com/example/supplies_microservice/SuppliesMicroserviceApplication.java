094248c13b0179e671c2a4f032ebacc913af9f9b
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-09-11 19:01:37 -0500

    Initial commit

---
+++ src/main/java/com/example/supplies_microservice/SuppliesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.supplies_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppliesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppliesMicroserviceApplication.class, args);
+	}
+
+}
