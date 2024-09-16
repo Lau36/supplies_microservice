3ae55694741e9970441503396035cc8d7b3e69af
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-09-15 20:20:32 -0500

    swagger documentation added

---
+++ src/main/java/com/example/transaction_microservice/infrastructure/configuration/OpenApiConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.transaction_microservice.infrastructure.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@OpenAPIDefinition(
+        info = @Info(
+                title = "TransactionsAPI",
+                version = "v1",
+                description = "Endpoint"
+        )
+)
+public class OpenApiConfiguration {
+}
